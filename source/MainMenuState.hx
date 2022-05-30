4352b659c7d324e5cc233ac4b31bc3b5f2cde519
G <gcgranja@gmail.com>
2022-05-30 20:04:03 +0100

    Test

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -215,7 +215,7 @@ class MainMenuState extends MusicBeatState
 			var menuItem:FlxSprite = new FlxSprite(100,100);
 			menuItem.scale.x = scale;
 			menuItem.scale.y = scale;
-			menuItem.frames = Paths.getSparrowAtlas('images/sick');
+			menuItem.frames = Paths.getSparrowAtlas('images/sick.png');
 			menuItem.animation.addByPrefix('idle', optionShit[6] + " basic", 24);
 			menuItem.animation.addByPrefix('selected', optionShit[6] + " white", 24);
 			menuItem.animation.play('idle');
